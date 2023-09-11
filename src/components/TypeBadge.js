a883e8bf26b4e071d88adb603f73b8576fa12579
Hernani Diniz <hdiniz4@gmail.com>
2023-09-11 14:29:51 +0100

    chore: add bg-color to the types

---
+++ src/components/TypeBadge.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './TypeBadge.css';
+
+
+const TypeBadge = ({ types }) => {
+    // Mapeie cada tipo para a classe de cor correspondente
+    const typeColors = types.map((type) => {
+      switch (type.type.name.toLowerCase()) {
+        case 'normal':
+          return 'bg-normal';
+        case 'fire':
+          return 'bg-fire';
+        case 'water':
+          return 'bg-water';
+        case 'electric':
+          return 'bg-electric';
+        case 'grass':
+          return 'bg-grass';
+        case 'ice':
+          return 'bg-ice';
+        case 'fighting':
+          return 'bg-fighting';
+        case 'poison':
+          return 'bg-poison';
+          case 'flying':
+          return 'bg-flying';
+        case 'ground':
+          return 'bg-ground';
+        case 'psychic':
+          return 'bg-psychic';
+        case 'bug':
+          return 'bg-bug';
+        case 'rock':
+          return 'bg-rock';
+        case 'ghost':
+          return 'bg-ghost';
+        case 'dragon':
+          return 'bg-dragon';
+        case 'fairy':
+          return 'bg-fairy';
+        default:
+          return 'bg-light-gray'; // Fallback para um fundo padrão.
+      }
+    });
+
+    return (
+        <div className="type-badges">
+          {types.map((type, index) => (
+            <div key={index} className={`type-badge ${typeColors[index]}`}>
+              {type.type.name}
+            </div>
+          ))}
+        </div>
+      );
+    };
+    
+    export default TypeBadge;
